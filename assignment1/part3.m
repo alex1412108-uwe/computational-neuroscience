7e63fda9a43ae6a53f5a1c4ddc687f538ed6dd3f
alex141108 <alex1412108@gmail.com>
2015-05-09 21:12:54 +0100

    fix location of t increase causing off by one error, fix fix exp to conform to normal matlab implementation, add plotting labels

--- assignment1/part3.m
+++ assignment1/part3.m
@@ -1,7 +1,7 @@
 function integrateandfiremodel
 %
-% part 2 of the neuron model
-% answer returned is 3.0000
+% part 3 of the neuron model
+% 
 %
 %?m = 10 [ms] (i.e. ?m is the time constant of the membrane)
 %EL = Vreset = -70 [mV]
@@ -21,27 +21,27 @@ Vzero = -70;
 Dt = 1; %ms
 t = 0;
 
-Ie = 0;
 
 V = zeros(1000,1);
 
 countspikes = zeros(31,1);
 
-counter = 0;
+counter = 1;
 
 for Ie=2:0.1:5,
     t = 0;
     for time=1:1000,
 
-        V(time) = EL + Rm * Ie + (Vzero - EL - Rm * Ie) * exp(1)^(-t/Tm);
+        V(time) = EL + Rm * Ie + (Vzero - EL - Rm * Ie) * exp(1) ^ (-t/Tm);
+        
+        % relative time
+        t = t + Dt;
+        
         if V(time) >= Vth
             V(time) = Vreset;
             t = 0;
             countspikes(counter) = countspikes(counter) + 1;
         end
-
-        % relative time
-        t = t + Dt;
 
 
     end
@@ -49,4 +49,6 @@ for Ie=2:0.1:5,
 end
 plotie = 2:0.1:5;
 plot(plotie, countspikes)
-countspikes
+title('spike frequency for various Ie values')
+xlabel('Time (ms)') % x-axis label
+ylabel('Voltage (mV)') % y-axis label
---
+++ assignment1/part4.m
@@ -0,0 +1,48 @@
+function integrateandfiremodel
+%
+% part 4 of the neuron model
+%
+
+
+Tm = 20;             %[ms] (i.e. ?m is the time constant of the membrane)
+EL = -70;            %[mV]
+Vreset = -80;        %[mV]
+Vth = -54;           %[mV]
+RmIe = 18;           %[mV]
+
+RmGs = 0.15;         %
+Pmax = 0.5;
+Ts = 10;             %[ms]
+Dt = 1;              %[ms]
+
+
+Vzero = Vreset;
+
+V = zeros(2,1000);
+
+
+% Vstart = randi([Vreset Vth],2,1)
+Vstart = randi([Vreset Vth],2,1);
+t = log((Vstart - EL - RmIe)/(Vzero - EL - RmIe)) * Tm * -1;
+
+
+%syncon = GsPmax
+
+for timer = 1:1000,
+    for neuron = 1:2,
+        V(neuron,timer) = EL + RmIe + (Vzero - EL - RmIe) * exp(-t(neuron)/Tm);
+        if V(neuron,timer) >= Vth
+            V(neuron,timer) = Vreset;
+            t(neuron) = 0;
+        end
+
+        % relative time
+        t(neuron) = t(neuron) + Dt;
+    end
+end
+plottime = 0:999;
+plot(plottime, V)
+title('voltage of a neuron')
+xlabel('Time in ms') % x-axis label
+ylabel('Voltage in mV') % y-axis label
+legend('y = sin(x)','y = cos(x)')
