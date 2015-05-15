a263409474c7c2b7021869713283cabad6e84759
alex141108 <alex1412108@gmail.com>
2015-05-15 21:39:10 +0100

    correctly labels the legend

--- assignment2/dataanalysis.m
+++ assignment2/dataanalysis.m
@@ -50,37 +50,45 @@ for i = testingtime
     if counttime1 <= neuron1cutoff(1)
         if i >= neuron1(counttime1)
             counttime1 = counttime1 + 1;
-            plot3(x(counttime),y(counttime),time(counttime),'.r')
+            dot1 = plot3(x(counttime),y(counttime),time(counttime),'.r');
         end
     end
     
     if counttime2 <= neuron2cutoff(1)
         if i >= neuron2(counttime2)
             counttime2 = counttime2 + 1;
-            plot3(x(counttime),y(counttime),time(counttime),'.g')
+            dot2 = plot3(x(counttime),y(counttime),time(counttime),'.g');
         end
     end
     
     if counttime3 <= neuron3cutoff(1)
         if i >= neuron3(counttime3)
             counttime3 = counttime3 + 1;
-            plot3(x(counttime),y(counttime),time(counttime),'.b')
+            dot3 = plot3(x(counttime),y(counttime),time(counttime),'.b');
         end
     end
     
     if counttime4 <= neuron4cutoff(1)
         if i >= neuron4(counttime4)
             counttime4 = counttime4 + 1;
-            plot3(x(counttime),y(counttime),time(counttime),'.y')
+            dot4 = plot3(x(counttime),y(counttime),time(counttime),'.y');
         end
     end
     
     counttime = counttime + 1;
 end
 
+title('Position of Neuron Spikes')
+xlabel('x position') % x-axis label
+ylabel('y position') % y-axis label
+zlabel('Time (1/10000 seconds)') % z-axis label
+legend([dot1, dot2, dot3, dot4],'neuron 1','neuron 2','neuron 3','neuron 4', 'Location', 'southwest') 
+%[h,icons,plots,str] = legend('neuron 1','neuron 2','neuron 3','neuron 4') %shows data about legend
 
-plot3(x,y,time,'k')
 
+dot5 = plot3(x,y,time,'k');
+
+legend([dot1, dot2, dot3, dot4, dot5],'neuron 1','neuron 2','neuron 3','neuron 4','position', 'Location', 'southwest') 
 printing = 'done'
 
 
