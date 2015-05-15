63b1adba0d7db27bd4925b040aad8b0668ece627
alex141108 <alex1412108@gmail.com>
2015-05-15 22:22:29 +0100

    add file to repo

---
+++ assignment2/differencebetweenspikes.m
@@ -0,0 +1,24 @@
+function differencebetweenspikes
+%
+% 
+% 
+%
+
+load 'data.mat'
+
+
+figure
+for i = neuron1
+    
+    
+end
+    
+title('Spike Time difference of Neuron1')
+xlabel('time (1/10000 seconds)') % x-axis label
+ylabel('number of spikes') % y-axis label
+
+
+ 
+printing = 'done'
+
+
