8f9fed2cb0b62384a24df2eee45d626eb8bc8b39
alex141108 <alex1412108@gmail.com>
2015-05-15 22:31:36 +0100

    for loop method, going to try matrix shifting instead

--- assignment2/differencebetweenspikes.m
+++ assignment2/differencebetweenspikes.m
@@ -7,12 +7,18 @@ function differencebetweenspikes
 load 'data.mat'
 
 
-figure
+pastspiketime = neuron1(1);
+sizeofneuron = size(neuron1);
+differenceinspikes = zeros(sizeofneuron(1)+1);
+
+count = 1;
 for i = neuron1
-    
-    
+    differenceinspikes(count) = neuron1
+    count = count + 1;
 end
-    
+
+figure
+
 title('Spike Time difference of Neuron1')
 xlabel('time (1/10000 seconds)') % x-axis label
 ylabel('number of spikes') % y-axis label
