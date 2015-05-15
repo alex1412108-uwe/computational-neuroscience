ab8cef5f89eaaf851c3bd1af6de6696735df169c
alex141108 <alex1412108@gmail.com>
2015-05-15 20:01:07 +0100

    less efficent method implemented, plots neuron spikes against the x,y,time graph (different color for each neuron)

--- assignment2/dataanalysis.m
+++ assignment2/dataanalysis.m
@@ -25,49 +25,22 @@ load 'data.mat'
 % legend('neuron 1','neuron 2','neuron 3','neuron 4')
 
 
-
-
-% figure
-% for i=1:countersize(2),
-%     
-%     if ismember(testingtime(i), testingneuron1)
-%         plot(x(i),y(i),'.r')
-%     end
-%     if ismember(testingtime(i), testingneuron2)
-%         plot(x(i),y(i),'.g')
-%     end
-%     if ismember(testingtime(i), testingneuron3)
-%         plot(x(i),y(i),'.b')
-%     end
-%     if ismember(testingtime(i), testingneuron4)
-%         plot(x(i),y(i),'.y')
-%     end
-% end
-% printing = 'done'
-
-% printing = 0;
-% 
-% countersize = (max(time) - min(time))/100;
-% counttime = 1;
-% 
-% figure
-% for i=min(time):max(time),
-%     if i >= time(counttime)
-%         plot3(x(counttime),y(counttime),time(counttime),'.g')
-%         counttime = counttime + 1;
-%     end
-% end
-
-
 testingtime = transpose(time);
 countersize = size(time);
 
 printing = 0;
 
 
-neuron1position = zeros(countersize(1),1);
+%neuron1position = zeros(countersize(1),1);
+
 neuron1cutoff = size(neuron1);
 counttime1 = 1;
+neuron2cutoff = size(neuron2);
+counttime2 = 1;
+neuron3cutoff = size(neuron3);
+counttime3 = 1;
+neuron4cutoff = size(neuron4);
+counttime4 = 1;
 
 counttime = 1;
 
@@ -76,22 +49,38 @@ hold on
 for i = testingtime
     if counttime1 <= neuron1cutoff(1)
         if i >= neuron1(counttime1)
-            neuron1position(counttime) = 1;
             counttime1 = counttime1 + 1;
-            plot3(x(counttime),y(counttime),time(counttime),'.g')
-        else
-            neuron1position(counttime) = 0;
+            plot3(x(counttime),y(counttime),time(counttime),'.r')
         end
     end
     
-
+    if counttime2 <= neuron2cutoff(1)
+        if i >= neuron2(counttime2)
+            counttime2 = counttime2 + 1;
+            plot3(x(counttime),y(counttime),time(counttime),'.g')
+        end
+    end
     
+    if counttime3 <= neuron3cutoff(1)
+        if i >= neuron3(counttime3)
+            counttime3 = counttime3 + 1;
+            plot3(x(counttime),y(counttime),time(counttime),'.b')
+        end
+    end
     
+    if counttime4 <= neuron4cutoff(1)
+        if i >= neuron4(counttime4)
+            counttime4 = counttime4 + 1;
+            plot3(x(counttime),y(counttime),time(counttime),'.y')
+        end
+    end
     
     counttime = counttime + 1;
 end
 
 
-plot3(x,y,time,'b')
+plot3(x,y,time,'k')
+
+printing = 'done'
 
 
