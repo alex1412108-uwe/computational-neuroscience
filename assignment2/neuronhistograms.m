72b36fd10a34b12af6777a6b28fbfcdce67a0d14
alex141108 <alex1412108@gmail.com>
2015-05-15 22:14:22 +0100

    finished histogram

--- assignment2/neuronhistograms.m
+++ assignment2/neuronhistograms.m
@@ -10,9 +10,26 @@ load 'data.mat'
 figure
 histogram(neuron1, 10000)
 title('Histogram of Neuron1 Spikes')
-xlabel('x position') % x-axis label
-ylabel('y position') % y-axis label
+xlabel('time (1/10000 seconds)') % x-axis label
+ylabel('number of spikes') % y-axis label
 
+figure
+histogram(neuron2, 10000)
+title('Histogram of Neuron2 Spikes')
+xlabel('time (1/10000 seconds)') % x-axis label
+ylabel('number of spikes') % y-axis label
+
+figure
+histogram(neuron3, 10000)
+title('Histogram of Neuron3 Spikes')
+xlabel('time (1/10000 seconds)') % x-axis label
+ylabel('number of spikes') % y-axis label
+
+figure
+histogram(neuron4, 10000)
+title('Histogram of Neuron4 Spikes')
+xlabel('time (1/10000 seconds)') % x-axis label
+ylabel('number of spikes') % y-axis label
  
 printing = 'done'
 
