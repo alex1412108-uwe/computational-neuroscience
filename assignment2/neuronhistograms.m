171903327bc382d5943290dbec6565d3514f4997
alex141108 <alex1412108@gmail.com>
2015-05-15 21:39:20 +0100

    add file to repo

---
+++ assignment2/neuronhistograms.m
@@ -0,0 +1,19 @@
+function integrateandfiremodel
+%
+% 
+% 
+%
+
+load 'data.mat'
+
+
+figure
+histogram(neuron1, 10000)
+title('Histogram of Neuron1 Spikes')
+xlabel('x position') % x-axis label
+ylabel('y position') % y-axis label
+
+ 
+printing = 'done'
+
+
